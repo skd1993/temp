47f1aa79b77bc0fc2973fde2913b60f77c705bac
Shobhit Kumar <shobhit.kr@outlook.com>
2022-04-18 09:48:53 +0000

    create mockend.js

---
+++ .mockend.js
@@ -0,0 +1,13 @@
+{
+  "User": {
+    "name": {
+      "string": "Shobhit"
+    },
+    "statusMessage": {
+      "string": [
+        "working from home",
+        "watching Netflix"
+      ]
+    },
+  }
+}
